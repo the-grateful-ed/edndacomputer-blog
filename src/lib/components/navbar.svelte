20b30c718ac6475f647821541250f17d5b0f4b68
the-grateful-ed <the-grateful-ed@riseup.net>
2022-05-08 23:33:17 -0700

    setup

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,20 @@
+<div
+  class="navbar mb-2 shadow-lg bg-neutral text-neutral-content rounded-box"
+>
+  <div class="flex-1 px-2 mx-2">
+    <span class="text-lg font-bold">Portfolio and Blog</span>
+  </div>
+  <div class="flex-none hidden px-2 mx-2 lg:flex">
+    <div class="flex items-stretch">
+      <a class="btn btn-ghost btn-sm rounded-btn" href="/projects">
+        Portfolio
+      </a>
+      <a class="btn btn-ghost btn-sm rounded-btn" href="/posts"
+        >Blog</a
+      >
+      <a class="btn btn-ghost btn-sm rounded-btn" href="/about"
+        >About</a
+      >
+    </div>
+  </div>
+</div>
